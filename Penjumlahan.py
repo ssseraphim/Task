30aa40518aa76bd282ddaf451cd85c6fa6829cdd
ssseraphim <mzehann22@gmail.com>
2025-01-29 18:37:26 +0700

    Add files via upload
    Task number 1

---
+++ Penjumlahan.py
@@ -0,0 +1,11 @@
+#Input
+angka1 = input("Masukkan angka pertama: ")
+angka2 = input("Masukkan angka kedua: ")
+
+#konversi
+angka1 = float(angka1)
+angka2 = float(angka2)
+jumlah = angka1 + angka2
+
+#Output
+print("Hasil penjumlahan:", jumlah)
